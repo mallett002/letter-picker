dd8fd50764a9d3fb1fc54a6dc920f62274125e32
william <wmallett@hy-vee.com>
2020-11-25 16:43:40 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Chance = require('chance');
+const say = require('say');
+const prompt = require('prompt-sync')();
+
+const chance = new Chance();
+
+const letters = ['J', 'T', 'M', 'O'];
+
+const ready = prompt('Ready? Enter, "y" ').toLocaleLowerCase();
+
+if (ready === 'y') {
+    setInterval(() => {
+        const letter = chance.pickone(letters);
+
+        console.log(letter);
+        say.speak(letter);
+    }, 3000);
+} else {
+    process.exit();
+}
